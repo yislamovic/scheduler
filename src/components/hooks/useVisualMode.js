e73144226cd79838c1720ebf995be7e37f98c579
Yahya Islamovic <yahya.islamovic.work@live.com>
2021-09-04 17:53:32 -0400

    created custom hook useVisualMode

---
+++ src/components/hooks/useVisualMode.js
@@ -0,0 +1,29 @@
+import { useState } from "react";
+function useCustomHook() {
+  function action() { }
+
+  return { action };
+}
+export default function useVisualMode(initial) {
+  const [mode, setMode] = useState(initial);
+  const [history, setHistory] = useState([initial]);
+  function transition(state, replace = false) {
+    if (!replace) {
+      setMode(() => state);
+      setHistory((prev) => [...prev, state]);
+    }
+    else{
+      setHistory((prev) => [...prev.slice(0, history.length - 1), state]);
+      setMode(() => state);
+    }
+  }
+  function back() {
+    if (history.length !== 1) {
+      setMode(() => {
+        return history[history.length - 2];
+      });
+      setHistory((prev) => [...prev.slice(0, history.length - 1)]);
+    }
+  }
+  return { mode, transition, back };
+}
